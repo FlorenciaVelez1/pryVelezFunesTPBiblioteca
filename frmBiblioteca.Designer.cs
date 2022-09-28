18511ecc550fda544288a9ed014813af7843b2ff
florencia <florenciavelez8@gmail.com>
2022-09-27 21:46:27 -0300

    Proyecto Terminado!

--- frmBiblioteca.Designer.cs
+++ frmBiblioteca.Designer.cs
@@ -28,62 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.listLibros = new System.Windows.Forms.ListBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmBiblioteca));
             this.cmdAnterior = new System.Windows.Forms.Button();
             this.cmdSiguiente = new System.Windows.Forms.Button();
+            this.lblCodigo = new System.Windows.Forms.Label();
+            this.lblEditorial = new System.Windows.Forms.Label();
+            this.lblNombreLibro = new System.Windows.Forms.Label();
+            this.lblCodigoAutor = new System.Windows.Forms.Label();
+            this.lblDistribuidor = new System.Windows.Forms.Label();
+            this.txtCodigoLibro = new System.Windows.Forms.TextBox();
+            this.txtEditorial = new System.Windows.Forms.TextBox();
+            this.txtNombreLibro = new System.Windows.Forms.TextBox();
+            this.txtCodigoAutor = new System.Windows.Forms.TextBox();
+            this.txtDistribuidor = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
-            // 
-            // listLibros
-            // 
-            this.listLibros.FormattingEnabled = true;
-            this.listLibros.ItemHeight = 16;
-            this.listLibros.Items.AddRange(new object[] {
-            "Código\tLibro\tCódigo Editorial\tCódigo Autor\tCódigo Distribuidor"});
-            this.listLibros.Location = new System.Drawing.Point(12, 12);
-            this.listLibros.Name = "listLibros";
-            this.listLibros.Size = new System.Drawing.Size(542, 228);
-            this.listLibros.TabIndex = 0;
             // 
             // cmdAnterior
             // 
-            this.cmdAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmdAnterior.Location = new System.Drawing.Point(12, 246);
+            this.cmdAnterior.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdAnterior.Location = new System.Drawing.Point(18, 276);
             this.cmdAnterior.Name = "cmdAnterior";
-            this.cmdAnterior.Size = new System.Drawing.Size(87, 34);
+            this.cmdAnterior.Size = new System.Drawing.Size(98, 34);
             this.cmdAnterior.TabIndex = 1;
             this.cmdAnterior.Text = "Anterior";
             this.cmdAnterior.UseVisualStyleBackColor = true;
+            this.cmdAnterior.Click += new System.EventHandler(this.cmdAnterior_Click);
             // 
             // cmdSiguiente
             // 
-            this.cmdSiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.cmdSiguiente.Location = new System.Drawing.Point(467, 246);
+            this.cmdSiguiente.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdSiguiente.Location = new System.Drawing.Point(314, 276);
             this.cmdSiguiente.Name = "cmdSiguiente";
-            this.cmdSiguiente.Size = new System.Drawing.Size(87, 34);
+            this.cmdSiguiente.Size = new System.Drawing.Size(98, 34);
             this.cmdSiguiente.TabIndex = 2;
             this.cmdSiguiente.Text = "Siguiente";
             this.cmdSiguiente.UseVisualStyleBackColor = true;
+            this.cmdSiguiente.Click += new System.EventHandler(this.cmdSiguiente_Click);
+            // 
+            // lblCodigo
+            // 
+            this.lblCodigo.AutoSize = true;
+            this.lblCodigo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.lblCodigo.Location = new System.Drawing.Point(20, 25);
+            this.lblCodigo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigo.Name = "lblCodigo";
+            this.lblCodigo.Size = new System.Drawing.Size(61, 20);
+            this.lblCodigo.TabIndex = 27;
+            this.lblCodigo.Text = "Código";
+            // 
+            // lblEditorial
+            // 
+            this.lblEditorial.AutoSize = true;
+            this.lblEditorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblEditorial.Location = new System.Drawing.Point(20, 123);
+            this.lblEditorial.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEditorial.Name = "lblEditorial";
+            this.lblEditorial.Size = new System.Drawing.Size(70, 20);
+            this.lblEditorial.TabIndex = 28;
+            this.lblEditorial.Text = "Editorial";
+            // 
+            // lblNombreLibro
+            // 
+            this.lblNombreLibro.AutoSize = true;
+            this.lblNombreLibro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombreLibro.Location = new System.Drawing.Point(20, 73);
+            this.lblNombreLibro.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblNombreLibro.Name = "lblNombreLibro";
+            this.lblNombreLibro.Size = new System.Drawing.Size(138, 20);
+            this.lblNombreLibro.TabIndex = 29;
+            this.lblNombreLibro.Text = "Nombre del Libro";
+            // 
+            // lblCodigoAutor
+            // 
+            this.lblCodigoAutor.AutoSize = true;
+            this.lblCodigoAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCodigoAutor.Location = new System.Drawing.Point(24, 173);
+            this.lblCodigoAutor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblCodigoAutor.Name = "lblCodigoAutor";
+            this.lblCodigoAutor.Size = new System.Drawing.Size(133, 20);
+            this.lblCodigoAutor.TabIndex = 30;
+            this.lblCodigoAutor.Text = "Código del Autor";
+            // 
+            // lblDistribuidor
+            // 
+            this.lblDistribuidor.AutoSize = true;
+            this.lblDistribuidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDistribuidor.Location = new System.Drawing.Point(20, 224);
+            this.lblDistribuidor.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblDistribuidor.Name = "lblDistribuidor";
+            this.lblDistribuidor.Size = new System.Drawing.Size(96, 20);
+            this.lblDistribuidor.TabIndex = 31;
+            this.lblDistribuidor.Text = "Distribuidor";
+            // 
+            // txtCodigoLibro
+            // 
+            this.txtCodigoLibro.Enabled = false;
+            this.txtCodigoLibro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoLibro.HideSelection = false;
+            this.txtCodigoLibro.Location = new System.Drawing.Point(181, 25);
+            this.txtCodigoLibro.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCodigoLibro.Name = "txtCodigoLibro";
+            this.txtCodigoLibro.Size = new System.Drawing.Size(231, 23);
+            this.txtCodigoLibro.TabIndex = 32;
+            // 
+            // txtEditorial
+            // 
+            this.txtEditorial.Enabled = false;
+            this.txtEditorial.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEditorial.Location = new System.Drawing.Point(181, 123);
+            this.txtEditorial.Margin = new System.Windows.Forms.Padding(4);
+            this.txtEditorial.Name = "txtEditorial";
+            this.txtEditorial.Size = new System.Drawing.Size(231, 23);
+            this.txtEditorial.TabIndex = 33;
+            // 
+            // txtNombreLibro
+            // 
+            this.txtNombreLibro.Enabled = false;
+            this.txtNombreLibro.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombreLibro.Location = new System.Drawing.Point(181, 72);
+            this.txtNombreLibro.Margin = new System.Windows.Forms.Padding(4);
+            this.txtNombreLibro.Multiline = true;
+            this.txtNombreLibro.Name = "txtNombreLibro";
+            this.txtNombreLibro.Size = new System.Drawing.Size(231, 21);
+            this.txtNombreLibro.TabIndex = 34;
+            // 
+            // txtCodigoAutor
+            // 
+            this.txtCodigoAutor.Enabled = false;
+            this.txtCodigoAutor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtCodigoAutor.Location = new System.Drawing.Point(181, 173);
+            this.txtCodigoAutor.Margin = new System.Windows.Forms.Padding(4);
+            this.txtCodigoAutor.Name = "txtCodigoAutor";
+            this.txtCodigoAutor.Size = new System.Drawing.Size(231, 23);
+            this.txtCodigoAutor.TabIndex = 35;
+            // 
+            // txtDistribuidor
+            // 
+            this.txtDistribuidor.Enabled = false;
+            this.txtDistribuidor.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDistribuidor.Location = new System.Drawing.Point(181, 219);
+            this.txtDistribuidor.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDistribuidor.Name = "txtDistribuidor";
+            this.txtDistribuidor.Size = new System.Drawing.Size(231, 23);
+            this.txtDistribuidor.TabIndex = 36;
             // 
             // frmBiblioteca
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(563, 292);
+            this.ClientSize = new System.Drawing.Size(435, 328);
+            this.Controls.Add(this.txtDistribuidor);
+            this.Controls.Add(this.txtCodigoAutor);
+            this.Controls.Add(this.txtNombreLibro);
+            this.Controls.Add(this.txtEditorial);
+            this.Controls.Add(this.txtCodigoLibro);
+            this.Controls.Add(this.lblDistribuidor);
+            this.Controls.Add(this.lblCodigoAutor);
+            this.Controls.Add(this.lblNombreLibro);
+            this.Controls.Add(this.lblEditorial);
+            this.Controls.Add(this.lblCodigo);
             this.Controls.Add(this.cmdSiguiente);
             this.Controls.Add(this.cmdAnterior);
-            this.Controls.Add(this.listLibros);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmBiblioteca";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Biblioteca";
             this.Load += new System.EventHandler(this.frmBiblioteca_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.ListBox listLibros;
         private System.Windows.Forms.Button cmdAnterior;
         private System.Windows.Forms.Button cmdSiguiente;
+        private System.Windows.Forms.Label lblCodigo;
+        private System.Windows.Forms.Label lblEditorial;
+        private System.Windows.Forms.Label lblNombreLibro;
+        private System.Windows.Forms.Label lblCodigoAutor;
+        private System.Windows.Forms.Label lblDistribuidor;
+        private System.Windows.Forms.TextBox txtCodigoLibro;
+        private System.Windows.Forms.TextBox txtEditorial;
+        private System.Windows.Forms.TextBox txtNombreLibro;
+        private System.Windows.Forms.TextBox txtCodigoAutor;
+        private System.Windows.Forms.TextBox txtDistribuidor;
     }
 }
 
--- frmBiblioteca.cs
+++ frmBiblioteca.cs
@@ -13,13 +13,109 @@ namespace pryVelezFunesTPBiblioteca
 {
     public partial class frmBiblioteca : Form
     {
+        //Declaracion de variables globales
+        public string[,] MatrizLibro = new string[20, 5];
+        int IndiceRecorrido = 0;
+        char Separador = Convert.ToChar(",");
         public frmBiblioteca()
         {
             InitializeComponent();
         }
         private void frmBiblioteca_Load(object sender, EventArgs e)
         {
-           
+            int i = 0;
+            StreamReader LectorLibro = new StreamReader("./LIBRO.txt");
+            while (!LectorLibro.EndOfStream)
+            {
+                //Separo los datos dentro del vector
+                string[] VectorLibro = LectorLibro.ReadLine().Split(Separador);
+                if (i < 20)
+                {
+                    //Cargar matriz
+                    MatrizLibro[i, 0] = VectorLibro[0];
+                    MatrizLibro[i, 1] = VectorLibro[1];
+                    MatrizLibro[i, 2] = VectorLibro[2];
+                    MatrizLibro[i, 3] = VectorLibro[3];
+                    MatrizLibro[i, 4] = VectorLibro[4];
+                    //Se llaman los procedimientos para cambiar los datos
+                    BuscarEditorial(i);
+                    BuscarDistribuidora(i);
+                    i++;
+                }
+            }
+            LectorLibro.Close();
+            txtCodigoLibro.Text = MatrizLibro[0, 0];
+            txtNombreLibro.Text = MatrizLibro[0, 1];
+            txtEditorial.Text = MatrizLibro[0, 2];
+            txtCodigoAutor.Text = MatrizLibro[0, 3];
+            txtDistribuidor.Text = MatrizLibro[0, 4];
+            cmdAnterior.Enabled = false;
         }
-    }
+        private void BuscarDistribuidora(int IndiceDistribuidor)
+        {
+            StreamReader LectorDistribuidora = new StreamReader("./DISTRIBUIDORA.txt");
+            while (!LectorDistribuidora.EndOfStream)
+            {
+                //Se compara los codigos del distribuidor
+                string[] vectorDistribuidor = LectorDistribuidora.ReadLine().Split(Separador);
+                //Se cambia el codigo por el nombre correspondiente al distribuidor
+                if (vectorDistribuidor[0] == MatrizLibro[IndiceDistribuidor, 4])
+                {
+                    MatrizLibro[IndiceDistribuidor, 4] = vectorDistribuidor[1];
+                }
+            }
+            LectorDistribuidora.Close();
+        }
+        private void BuscarEditorial(int IndiceEditorial)
+        {
+            StreamReader LectorEditorial = new StreamReader("./EDITORIAL.txt");
+            while (!LectorEditorial.EndOfStream)
+            {
+                //Se comparan los codigos del editorial
+                string[] vectorEditorial = LectorEditorial.ReadLine().Split(Separador);
+                if (vectorEditorial[0] == MatrizLibro[IndiceEditorial, 2])
+                {
+                    MatrizLibro[IndiceEditorial, 2] = vectorEditorial[1];
+                }
+            }
+            LectorEditorial.Close();
+        }
+        private void cmdSiguiente_Click(object sender, EventArgs e)
+        {
+            IndiceRecorrido++;
+            txtCodigoLibro.Text = MatrizLibro[IndiceRecorrido, 0];
+            txtNombreLibro.Text = MatrizLibro[IndiceRecorrido, 1];
+            txtEditorial.Text = MatrizLibro[IndiceRecorrido, 2];
+            txtCodigoAutor.Text = MatrizLibro[IndiceRecorrido, 3];
+            txtDistribuidor.Text = MatrizLibro[IndiceRecorrido, 4];
+            cmdAnterior.Enabled = true;
+            if (IndiceRecorrido == MatrizLibro.GetLength(0) - 1)
+            {
+                cmdSiguiente.Enabled = false;
+            }
+        }
+        private void cmdAnterior_Click(object sender, EventArgs e)
+        {
+            //Resta 1 al indice
+            IndiceRecorrido--;
+            //Si es mayor o igual resta 1
+            if (IndiceRecorrido >= 0) 
+            {
+
+                txtCodigoLibro.Text = MatrizLibro[IndiceRecorrido, 0];
+                txtNombreLibro.Text = MatrizLibro[IndiceRecorrido, 1];
+                txtEditorial.Text = MatrizLibro[IndiceRecorrido, 2];
+                txtCodigoAutor.Text = MatrizLibro[IndiceRecorrido, 3];
+                txtDistribuidor.Text = MatrizLibro[IndiceRecorrido, 4];
+                if (IndiceRecorrido == 0)
+                {
+                    cmdAnterior.Enabled = false;
+                }
+            }
+            else
+            {
+                cmdAnterior.Enabled = false;
+            }
+        }
+    }   
 }
